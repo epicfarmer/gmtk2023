a03f2092e7836ad349366e4daf95ed6b70896a71
jkamins7 <jkaminsky@jhu.edu>
2023-07-07 10:36:49 -1000

    Switched to _input for processing input for my sanity

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var speed = 10  # speed in squares/sec
 var velocity = Vector2.ZERO
+var input = Vector2.ZERO
 # goin to use these to determine animation state, etc.
 enum control_states {CONTROLLED, UNCONTROLLED}
 var current_state = control_states.UNCONTROLLED
@@ -14,29 +15,33 @@ func set_controlled():
 func set_uncontrolled():
 	current_state = control_states.UNCONTROLLED
 
-func get_input():
-	if Input.is_action_just_pressed("ui_select"):
-		if current_state == control_states.CONTROLLED:
-			current_state = control_states.UNCONTROLLED
-		else:
-			current_state = control_states.CONTROLLED
-		print("Current state is ", current_state)
+func _input(event):
 	if current_state == control_states.CONTROLLED:
-		process_input()
+		process_input(event)
 
-func process_input():
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed('right'):
-		velocity.x = 1
-	if Input.is_action_pressed('left'):
-		velocity.x = -1
-	if Input.is_action_pressed('down'):
-		velocity.y = 1
-	if Input.is_action_pressed('up'):
-		velocity.y = -1
+
+func process_input(event):
+
+	if event is InputEventKey:
+		if event.is_action_pressed('right'):
+			input.x += 1
+		if event.is_action_pressed('left'):
+			input.x += -1
+		if event.is_action_pressed('down'):
+			input.y += 1
+		if event.is_action_pressed('up'):
+			input.y += -1
+		if event.is_action_released('right'):
+
+			input.x -= 1
+		if event.is_action_released('left'):
+			input.x -= -1
+		if event.is_action_released('down'):
+			input.y -= 1
+		if event.is_action_released('up'):
+			input.y -= -1
 	# Make sure diagonal movement isn't faster
-	velocity = velocity.normalized() * speed * grid_size
+	velocity = input.normalized() * speed * grid_size
 
 func _physics_process(_delta):
-	get_input()
 	velocity = move_and_slide(velocity)
