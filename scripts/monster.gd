eb7823ab27ceb9429d80b0e0dbde1db66c4d8f41
jkamins7 <jkaminsky@jhu.edu>
2023-07-07 10:52:11 -1000

    Updated player controller. Adjusted monsters to be able to go through doors.

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -10,9 +10,14 @@ var current_state = control_states.UNCONTROLLED
 var grid_size = 16
 
 func set_controlled():
+	input = Vector2(
+		float(Input.is_action_pressed("right")) - float(Input.is_action_pressed("left")),
+		float(Input.is_action_pressed("down")) - float(Input.is_action_pressed("up"))
+	)
 	current_state = control_states.CONTROLLED
 	
 func set_uncontrolled():
+	input = Vector2.ZERO
 	current_state = control_states.UNCONTROLLED
 
 func _input(event):
@@ -32,7 +37,6 @@ func process_input(event):
 		if event.is_action_pressed('up'):
 			input.y += -1
 		if event.is_action_released('right'):
-
 			input.x -= 1
 		if event.is_action_released('left'):
 			input.x -= -1
