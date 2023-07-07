456e8d07c46a9f5e6c8ec604ebbad303b03802ef
jkamins7 <jkaminsky@jhu.edu>
2023-07-07 08:37:32 -1000

    Added basic monster

--- scripts/basicmovement.gd
+++ scripts/basicmovement.gd
@@ -15,16 +15,17 @@ func pick_next_action():
 func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('right'):
-		velocity.x += 1
+		velocity.x = 1
 	if Input.is_action_pressed('left'):
-		velocity.x -= 1
+		velocity.x = -1
 	if Input.is_action_pressed('down'):
-		velocity.y += 1
+		velocity.y = 1
 	if Input.is_action_pressed('up'):
-		velocity.y -= 1
+		velocity.y = -1
 	# Make sure diagonal movement isn't faster
 	velocity = velocity.normalized() * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+
