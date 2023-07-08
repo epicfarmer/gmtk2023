59fecccb5005c98f7d037e754c2e1fe40d071512
jkamins7 <jkaminsky@jhu.edu>
2023-07-08 13:00:27 -1000

    Fixed a bug with selection

--- scripts/CursorSelector.gd
+++ scripts/CursorSelector.gd
@@ -15,12 +15,14 @@ func _input(event):
 	if event is InputEventMouseButton:
 		var viewport = get_viewport()
 		position = viewport.get_mouse_position() + get_parent().get_camera_offset()
-		currently_selected.set_uncontrolled()
+		if is_instance_valid(currently_selected):
+			currently_selected.set_uncontrolled()
 		var other_bodies = get_overlapping_bodies()
 		if len(other_bodies) > 0:
 			other_bodies[0].set_controlled()
 			other_bodies[0].select(self)
 			currently_selected = other_bodies[0]
+			print("Selecting ", currently_selected)
 
 func reset_selected():
 	currently_selected = self
