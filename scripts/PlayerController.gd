0af3e520dc30e96d9040f96530a76820519cb835
jkamins7 <jkaminsky@jhu.edu>
2023-07-08 10:58:03 -1000

    Camera now moves based on rooms.

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -1,4 +1,9 @@
 extends Node
 
+onready var camera = $Camera
+
 func get_controlled_monster():
 	return get_node("CursorSelector").currently_selected
+
+func get_camera_offset():
+	return camera.transform.get_origin()
