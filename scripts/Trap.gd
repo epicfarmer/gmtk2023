0adeeb0443a19c613d78b0280bed576b4ce1adf6
mjbry17 <bee.rye.ant@gmail.com>
2023-07-09 01:07:37 +0200

    spike trap

---
+++ scripts/Trap.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+onready var sprite = $Sprite
+
+func _ready():
+	self.connect("body_entered", self, "_on_Hitbox_body_entered")
+	self.connect("body_exited", self, "_on_Hitbox_body_exited")
+
+func _on_Hitbox_body_entered(_body):
+	sprite.set_frame(388)
+	_body.take_damage()
+
+func _on_Hitbox_body_exited(_body):
+	sprite.set_frame(385)
