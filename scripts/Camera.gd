a499c16662278d1f410f04c1b283b11e19723347
jkamins7 <jkaminsky@jhu.edu>
2023-07-09 00:56:29 -1000

    Fixing camera movement.

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -2,4 +2,4 @@ extends Camera2D
 
 func _on_hero_room_entered(_room):
 	print("Entering room")
-	position = _room.get_position() - Vector2(100,100)# - Vector2f(width/2, height/2)
+	position = _room.get_global_position() - Vector2(136,72)# - Vector2f(width/2, height/2)
